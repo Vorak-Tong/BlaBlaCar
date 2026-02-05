41be21bd90789fbafeee852622a4fd3c0e327817
Vorak Tong <viraktong197@gmail.com>
2026-02-05 20:21:20 +0700

    BLA-000- Start Code

---
+++ test/test_rides.dart
@@ -0,0 +1,14 @@
+// import 'package:blabla/models/ride/ride.dart';
+
+// import 'package:blabla/models/ride/locations.dart';
+// import 'package:blabla/services/rides_service.dart';
+
+// void main() {
+//   Location dijon = Location(country: Country.france, name: "Dijon");
+
+//   List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
+
+//   for (Ride ride in filteredRide) {
+//     print(ride);
+//   }
+// }
